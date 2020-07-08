b22ffcea374331ad64ce77bdb4e35d9e7c12891e
BingShunDuan <568646781@qq.com>
2020-07-08 19:55:19 +0800

    add new line by idea

--- testgit/src/cn/itcast/git/HelloGit.java
+++ testgit/src/cn/itcast/git/HelloGit.java
@@ -3,5 +3,6 @@ package cn.itcast.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("this is my first git");
+        System.out.println("version2");
     }
 }
